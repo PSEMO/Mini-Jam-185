25bf44931c8416834dbe3c55fd876be1bddfba57
Gabriel Ramalho <gabrielramalhorg@gmail.com>
2025-05-24 20:33:41 -0300

    Level designs and some small changes

--- Assets/Scripts/MoveVerticalSingleDirection.cs
+++ Assets/Scripts/MoveVerticalSingleDirection.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MoveVerticalSingleDirection : MonoBehaviour
 {
-    private float moveSpeed = 5f;
-
+    [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float currentDirection = 1; // 1 up, -1 down
+    private Renderer objectRenderer;
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();    
+    }
+
 
     void Update()
     {
-        transform.Translate(Vector3.up * currentDirection * moveSpeed * Time.deltaTime);
+        if (objectRenderer.isVisible)
+        {
+            transform.Translate(Vector3.up * currentDirection * moveSpeed * Time.deltaTime);
+        }
     }
 
     private void ReverseDirection()
